7fccfb1df8368f3d9aac0291199a43f481d54be3
Alina Drebin <alina@drebin.com>
2024-02-14 20:32:55 -0800

    add row padding

--- my-app/src/Row.js
+++ my-app/src/Row.js
@@ -22,15 +22,15 @@ function Row(props) {
 		switch (index) {
 			case 0:
 				return COLOR_WHEEL[1];
-			case 1: 
+			case 1:
 				return COLOR_WHEEL[2];
-			case 2: 
+			case 2:
 				return COLOR_WHEEL[0];
 		}
 	}
 
 	const handleSubmit = (e) => {
-    e.preventDefault();
+		e.preventDefault();
 
 		const formattedData = {
 			1: {
@@ -59,29 +59,29 @@ function Row(props) {
 	}
 
 	const handleOnChange = (e, name) => {
-		setFormData({...formData, [name]: e.target.value.toUpperCase()});
+		setFormData({ ...formData, [name]: e.target.value.toUpperCase() });
 	}
 
-  return (
-    <form onSubmit={handleSubmit} autoComplete="off">
-			<input type="text" name="one" className={formData.color1} onChange={(e) => handleOnChange(e, 'one')} value={formData.one}/>
-			<input type="button" name="color1" onClick={() => setFormData({...formData, color1: rotateColor(formData.color1)})}/>
+	return (
+		<form className="row" onSubmit={handleSubmit} autoComplete="off">
+			<input type="text" name="one" className={formData.color1} onChange={(e) => handleOnChange(e, 'one')} value={formData.one} />
+			<input type="button" name="color1" onClick={() => setFormData({ ...formData, color1: rotateColor(formData.color1) })} />
 
-			<input type="text" name="two" className={formData.color2} onChange={(e) => handleOnChange(e, 'two')} value={formData.two}/>
-			<input type="button" name="color2" onClick={() => setFormData({...formData, color2: rotateColor(formData.color2)})}/>
+			<input type="text" name="two" className={formData.color2} onChange={(e) => handleOnChange(e, 'two')} value={formData.two} />
+			<input type="button" name="color2" onClick={() => setFormData({ ...formData, color2: rotateColor(formData.color2) })} />
 
-			<input type="text" name="three" className={formData.color3} onChange={(e) => handleOnChange(e, 'three')} value={formData.three}/>
-			<input type="button" name="color3" onClick={() => setFormData({...formData, color3: rotateColor(formData.color3)})}/>
+			<input type="text" name="three" className={formData.color3} onChange={(e) => handleOnChange(e, 'three')} value={formData.three} />
+			<input type="button" name="color3" onClick={() => setFormData({ ...formData, color3: rotateColor(formData.color3) })} />
 
-			<input type="text" name="four" className={formData.color4} onChange={(e) => handleOnChange(e, 'four')} value={formData.four}/>
-			<input type="button" name="color4" onClick={() => setFormData({...formData, color4: rotateColor(formData.color4)})}/>
+			<input type="text" name="four" className={formData.color4} onChange={(e) => handleOnChange(e, 'four')} value={formData.four} />
+			<input type="button" name="color4" onClick={() => setFormData({ ...formData, color4: rotateColor(formData.color4) })} />
 
-			<input type="text" name="five" className={formData.color5} onChange={(e) => handleOnChange(e, 'five')} value={formData.five}/>
-			<input type="button" name="color5" onClick={() => setFormData({...formData, color5: rotateColor(formData.color5)})}/>
+			<input type="text" name="five" className={formData.color5} onChange={(e) => handleOnChange(e, 'five')} value={formData.five} />
+			<input type="button" name="color5" onClick={() => setFormData({ ...formData, color5: rotateColor(formData.color5) })} />
 
 			<input type="submit" value="Submit" />
 		</form>
-  );
+	);
 }
 
 export default Row;
