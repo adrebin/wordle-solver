d10c4f6bc1fea621deb242d1d0980487c28baa1c
Alina Drebin <alina@drebin.com>
2024-02-29 22:00:19 -0800

    refactor to add tests

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,95 @@ import { findAnagrams } from './anagram';
 import Row from './Row';
 import AnagramsList from './AnagramsList';
 
+
+function getLetters(absolute_nots, regex_musts) {
+  // remove all "gray" (absolute nots) letters from the alphabet
+  let letters = "abcdefghijklmnopqrstuvwxyz";
+  // find all "gray" (absolute nots) that are also greens -> these are technically allowed!
+  let fake_grays = [...absolute_nots.join("")].filter(x => [...regex_musts.join("")].includes(x));
+  let difference = [...absolute_nots.join("")].filter(x => ![...regex_musts.join("")].includes(x) && !fake_grays.includes(x));
+  for (let l = 0; l < difference.length; l++) {
+    letters = letters.replace(difference[l], "");
+  }
+  return letters;
+}
+
+function getAnagrams(letters, regex_musts, regex_nots) {
+  // build up regex + must contains list
+  // by condensing each column into regex
+  let regex = "";
+  let must_contain = ""
+  for (let s = 0; s < 5; s++) {
+    must_contain = must_contain + regex_nots[s];
+    if (regex_musts[s]) {
+      //all greens 
+      regex = regex + "[" + regex_musts[s] + "]";
+      must_contain = must_contain + regex_musts[s];
+    } else if (regex_nots[s] != "") {
+      // all yellows
+      regex = regex + "[^" + regex_nots[s] + "]";
+    } else {
+      // if null or "", add .
+      regex = regex + ".";
+    }
+  }
+
+  // get unique must_contains list in case duplicates
+  must_contain = Array.from(new Set([...must_contain])).join("");
+  return findAnagrams(letters, "^" + regex + "$", must_contain);
+}
+
+export function calculateAnagramsFromGameData(tempGameData) {
+  // letters that are grey
+  // each entry here represents the concatenation of the column
+  let absolute_nots = ["", "", "", "", ""];
+  // letters that are yellow
+  // each entry here represents the concatenation of the column
+  let regex_nots = ["", "", "", "", ""];
+  // letters that are green
+  // each entry here represents the concatenation of the column
+  let regex_musts = [null, null, null, null, null];
+  // iterate through each row
+  for (let i = 1; i <= 6; i++) {
+    let rowData = tempGameData[i];
+    if (Object.keys(rowData).length) {
+      // find all the colors for each letter in the row
+      // to weed out fake grays due to duplicate letters
+      const letterToColor = {};
+      for (const val of Object.values(rowData)) {
+        if (Object.keys(letterToColor).includes(val.value)) {
+          letterToColor[val.value].push(val.color);
+        } else {
+          letterToColor[val.value] = [val.color];
+        }
+      }
+
+      for (const [key, value] of Object.entries(rowData)) {
+        switch (value.color) {
+          case "gray":
+            if (letterToColor[value.value].includes('yellow') || letterToColor[value.value].includes('green')) {
+              // indicates this is a fake gray - it is yellow or green elsewhere in the row
+              // and we can't exclude the letter yet
+              break;
+            }
+            absolute_nots[key - 1] = absolute_nots[key - 1] + value.value;
+            break;
+          case "yellow":
+            regex_nots[key - 1] = regex_nots[key - 1] + value.value;
+            break;
+          default:
+            regex_musts[key - 1] = value.value;
+        }
+      }
+    }
+  }
+
+  const letters = getLetters(absolute_nots, regex_musts);
+  const anagrams = [...getAnagrams(letters, regex_musts, regex_nots)];
+  return anagrams;
+};
+
+
 function App() {
   const [gameData, setGameData] = useState({
     1: {},
@@ -18,92 +107,11 @@ function App() {
 
   const [anagrams, setAnagrams] = useState();
 
-  const getLetters = (absolute_nots, regex_musts) => {
-    // remove all "gray" (absolute nots) letters from the alphabet
-    let letters = "abcdefghijklmnopqrstuvwxyz";
-    // find all "gray" (absolute nots) that are also greens -> these are technically allowed!
-    let fake_grays = [...absolute_nots.join("")].filter(x => [...regex_musts.join("")].includes(x));
-    let difference = [...absolute_nots.join("")].filter(x => ![...regex_musts.join("")].includes(x) && !fake_grays.includes(x));
-    for (let l = 0; l < difference.length; l++) {
-      letters = letters.replace(difference[l], "");
-    }
-    return letters;
-  }
-
-  const getAnagrams = (letters, regex_musts, regex_nots) => {
-    // build up regex + must contains list
-    // by condensing each column into regex
-    let regex = "";
-    let must_contain = ""
-    for (let s = 0; s < 5; s++) {
-      must_contain = must_contain + regex_nots[s];
-      if (regex_musts[s]) {
-        //all greens 
-        regex = regex + "[" + regex_musts[s] + "]";
-        must_contain = must_contain + regex_musts[s];
-      } else if (regex_nots[s] != "") {
-        // all yellows
-        regex = regex + "[^" + regex_nots[s] + "]";
-      } else {
-        // if null or "", add .
-        regex = regex + ".";
-      }
-    }
-
-    // get unique must_contains list in case duplicates
-    must_contain = Array.from(new Set([...must_contain])).join("");
-    return findAnagrams(letters, "^" + regex + "$", must_contain);
-  }
 
   const setRowData = (row, data) => {
     const tempGameData = { ...gameData, [row]: data };
-
-    // letters that are grey
-    // each entry here represents the concatenation of the column
-    let absolute_nots = ["", "", "", "", ""];
-    // letters that are yellow
-    // each entry here represents the concatenation of the column
-    let regex_nots = ["", "", "", "", ""];
-    // letters that are green
-    // each entry here represents the concatenation of the column
-    let regex_musts = [null, null, null, null, null];
-    // iterate through each row
-    for (let i = 1; i <= 6; i++) {
-      let rowData = tempGameData[i];
-      if (Object.keys(rowData).length) {
-        // find all the colors for each letter in the row
-        // to weed out fake grays due to duplicate letters
-        const letterToColor = {};
-        for (const val of Object.values(rowData)) {
-          if (Object.keys(letterToColor).includes(val.value)) {
-            letterToColor[val.value].push(val.color);
-          } else {
-            letterToColor[val.value] = [val.color];
-          }
-        }
-
-        for (const [key, value] of Object.entries(rowData)) {
-          switch (value.color) {
-            case "gray":
-              if (letterToColor[value.value].includes('yellow') || letterToColor[value.value].includes('green')) {
-                // indicates this is a fake gray - it is yellow or green elsewhere in the row
-                // and we can't exclude the letter yet
-                break;
-              }
-              absolute_nots[key - 1] = absolute_nots[key - 1] + value.value;
-              break;
-            case "yellow":
-              regex_nots[key - 1] = regex_nots[key - 1] + value.value;
-              break;
-            default:
-              regex_musts[key - 1] = value.value;
-          }
-        }
-      }
-    }
-
-    const letters = getLetters(absolute_nots, regex_musts);
-    setAnagrams([...getAnagrams(letters, regex_musts, regex_nots)]);
+    const anagrams = calculateAnagramsFromGameData(tempGameData);
+    setAnagrams(anagrams);
     setGameData(tempGameData);
   };
 
--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,140 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { calculateAnagramsFromGameData } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// Make it easier to generate test cases
+function generateGameData(specifiedRows) {
+  let gameData = {
+    1: {},
+    2: {},
+    3: {},
+    4: {},
+    5: {},
+    6: {},
+  };
+
+  for (let i = 0; i < specifiedRows.length; i++) {
+    const row = {};
+    let counter = 1;
+    for (const [letter, color] of specifiedRows[i]) {
+      row[counter] = { value: letter, color: color }
+      counter++;
+    }
+    gameData[i + 1] = row;
+  }
+  return gameData;
+}
+
+test('random case 1', () => {
+  const specifiedRows = [
+    [["r", "yellow"], ["a", "gray"], ["i", "gray"], ["s", "gray"], ["e", "yellow"]],
+    [["c", "gray"], ["l", "green"], ["o", "gray"], ["u", "gray"], ["t", "gray"]],
+    [["n", "gray"], ["y", "gray"], ["m", "gray"], ["p", "gray"], ["h", "gray"]],
+  ];
+
+  const gameData = generateGameData(specifiedRows);
+  const anagrams = calculateAnagramsFromGameData(gameData);
+  expect(anagrams).toEqual(["elder", "elver", "fleer"]);
+});
+
+test('random case 2', () => {
+  const specifiedRows = [
+    [["o", "gray"], ["r", "gray"], ["d", "gray"], ["e", "green"], ["r", "gray"]],
+    [["p", "gray"], ["s", "yellow"], ["y", "gray"], ["c", "gray"], ["h", "gray"]],
+    [["g", "gray"], ["a", "gray"], ["m", "gray"], ["e", "green"], ["s", "yellow"]],
+  ];
+
+  const gameData = generateGameData(specifiedRows);
+  const anagrams = calculateAnagramsFromGameData(gameData);
+  expect(anagrams).toEqual([
+    'besee', 'beset', 'eisel', 'ensew',
+    'fusee', 'fusel', 'inset', 'nisei',
+    'seven', 'sewel', 'sewen', 'silen',
+    'silex', 'sinew', 'sizel', 'skeef',
+    'skeen', 'skeet', 'sleek', 'sleet',
+    'steek', 'steel', 'steen', 'sujee',
+    'sweel', 'sweet', 'unsee', 'unset',
+    'unsew', 'unsex'
+  ]);
+});
+
+test('random case 3', () => {
+  const specifiedRows = [
+    [["c", "gray"], ["r", "gray"], ["a", "gray"], ["n", "gray"], ["e", "green"]],
+    [["h", "gray"], ["o", "gray"], ["i", "gray"], ["s", "gray"], ["t", "gray"]],
+    [["b", "gray"], ["u", "yellow"], ["l", "yellow"], ["k", "gray"], ["y", "gray"]],
+    [["l", "yellow"], ["e", "yellow"], ["d", "yellow"], ["g", "gray"], ["e", "green"]],
+  ];
+
+  const gameData = generateGameData(specifiedRows);
+  const anagrams = calculateAnagramsFromGameData(gameData);
+  expect(anagrams).toEqual(["elude"]);
+});
+
+test('random case 4', () => {
+  const specifiedRows = [
+    [["o", "gray"], ["p", "gray"], ["e", "yellow"], ["n", "gray"], ["s", "gray"]],
+    [["b", "yellow"], ["a", "yellow"], ["b", "green"], ["e", "green"], ["s", "gray"]],
+    [["k", "gray"], ["e", "yellow"], ["b", "green"], ["a", "yellow"], ["b", "yellow"]],
+  ];
+
+  const gameData = generateGameData(specifiedRows);
+  const anagrams = calculateAnagramsFromGameData(gameData);
+  expect(anagrams).toEqual(['abbed', 'abbey', 'albee', 'amber']);
+});
+
+test('random case 5', () => {
+  const specifiedRows = [
+    [["n", "gray"], ["a", "gray"], ["v", "gray"], ["a", "gray"], ["l", "yellow"]],
+    [["e", "green"], ["v", "gray"], ["a", "gray"], ["d", "yellow"], ["e", "yellow"]],
+    [["f", "gray"], ["l", "green"], ["u", "gray"], ["f", "gray"], ["f", "gray"]],
+    [["s", "gray"], ["u", "gray"], ["n", "gray"], ["n", "gray"], ["y", "gray"]],
+    [["t", "gray"], ["h", "gray"], ["r", "yellow"], ["e", "green"], ["e", "yellow"]],
+    [["p", "gray"], ["a", "gray"], ["d", "green"], ["d", "gray"], ["y", "gray"]],
+  ];
+
+  const gameData = generateGameData(specifiedRows);
+  const anagrams = calculateAnagramsFromGameData(gameData);
+  expect(anagrams).toEqual(['elder']);
+});
+
+test('random case 6', () => {
+  const specifiedRows = [
+    [["a", "green"], ["l", "gray"], ["g", "gray"], ["a", "gray"], ["e", "yellow"]],
+    [["k", "gray"], ["e", "yellow"], ["e", "gray"], ["p", "gray"], ["s", "gray"]],
+    [["o", "gray"], ["r", "gray"], ["b", "green"], ["i", "gray"], ["t", "gray"]],
+    [["a", "green"], ["b", "green"], ["a", "gray"], ["t", "gray"], ["e", "yellow"]],
+  ];
+
+  const gameData = generateGameData(specifiedRows);
+  const anagrams = calculateAnagramsFromGameData(gameData);
+  console.log(anagrams)
+  expect(anagrams).toEqual(['abbed', 'abbey']);
+});
+
+test('weird duplicate letters edge case 1', () => {
+  const specifiedRows = [
+    [["a", "yellow"], ["d", "gray"], ["i", "yellow"], ["e", "yellow"], ["u", "gray"]],
+    [["t", "gray"], ["a", "yellow"], ["m", "yellow"], ["a", "gray"], ["l", "gray"]],
+  ];
+
+  const gameData = generateGameData(specifiedRows);
+  const anagrams = calculateAnagramsFromGameData(gameData);
+  expect(anagrams).toEqual(["image", "minae"]);
+});
+
+test('weird duplicate letters edge case 2', () => {
+  const specifiedRows = [
+    [["a", "yellow"], ["n", "gray"], ["n", "green"], ["a", "green"], ["l", "green"]],
+    [["u", "gray"], ["n", "yellow"], ["i", "gray"], ["o", "gray"], ["n", "gray"]],
+    [["a", "yellow"], ["l", "yellow"], ["l", "gray"], ["o", "gray"], ["y", "gray"]],
+  ];
+
+  const gameData = generateGameData(specifiedRows);
+  const anagrams = calculateAnagramsFromGameData(gameData);
+  expect(anagrams).toEqual(["banal", "canal", "fanal", "genal", "penal", "renal", "venal"]);
 });
